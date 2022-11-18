0dd129cb1090d637115ae28bef1c2df47196451f
Constanca Casimiro <duarte.connie2003@gmail.com>
2022-11-18 11:17:29 +0000

    ready for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get 'lists', to: 'lists#index'
+  # get 'lists/:id', to: 'lists#show'
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id/bookmarks/new', to: 'bookmarks#new'
+  # post 'lists/:id/bookmarks', to: 'bookmarks#create'
+
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  delete 'bookmarks/:id', to: 'bookmarks#destroy', as: :bookmark
 end
